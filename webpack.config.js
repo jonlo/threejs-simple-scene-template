1cba6f3f1f9242b69fef869dc037315ce8bd4862
Jon Lopez de Guereña <jon.lopezdeguerena@tecnalia.com>
2022-04-23 12:19:12 +0200

    updated to webpack 5 and three 139

--- webpack.config.js
+++ webpack.config.js
@@ -1,115 +1,134 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable no-undef */
+/* COMMENTS AND TUTORIALS
+ Development live debugging with webpack-dev-server:
+ https://desarrolloweb.com/articulos/servidor-desarrollo-webpack.html
+ https://webpack.js.org/configuration/dev-server/
+
+ */
+
 var path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const pkg = require('./package.json');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-var devMode = true;
+const devMode = process.env.NODE_ENV === 'development';
+module.exports = (env) => {
 
-module.exports = {
-	entry: ['babel-polyfill', './src/index.js', './src/styles/main.css'],
-	watch: true,
-	devtool: 'inline-source-map',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'index_bundle.js'
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'dist'),
-		compress: true,
-		port: 9000
-	},
-	module: {
-		rules: [{
-			test: /\.m?js$/,
-			exclude: /(node_modules|bower_components)/,
-			use: {
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-				}
-			}
-		},
-		{
-			test: /\.html$/,
-			use: {
-				loader: 'html-loader',
-				options: {
-					minimize: false,
-					sourceMap: true
-				}
-			}
-		},
-		{
-			test: /\.css$/,
-			use: [{
-				loader: MiniCssExtractPlugin.loader,
-				options: {
-					// only enable hot in development
-					hmr: devMode,
-					// if hmr does not work, this is a forceful method.
-					reloadAll: true
-				}
-			},
-			{
-				loader: 'css-loader',
-				options: {
-					sourceMap: devMode
-				}
-			}
-			]
-		},
-		{
-			test: /\.(gif|png|jpe?g|svg)$/i,
-			use: [{
-				loader: 'image-webpack-loader',
-				options: {
-					bypassOnDebug: true, // webpack@1.x
-					disable: true // webpack@2.x and newer
-				}
-			},
-			{
-				loader: 'url-loader',
-				options: {
-					limit: 8192
-				}
-			}
-			]
-		},
-		{
-			test: /\.ttf$/,
-			use: [{
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-					outputPath: 'fonts/'
-				}
-			}]
-			/*use: [
-				{
-					loader: 'ttf-loader',
-					options: {
-						name: './font/[hash].[ext]'
-					}
-				}
-			]*/
+
+	return {
+		entry: {
+			mn: ['./src/index.js', './src/styles/main.css']
 		}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-			filename: "./index.html"
-		}),
-		new MiniCssExtractPlugin({
-			filename: "[name].css",
-			chunkFilename: "[id].css"
-		}),
-		new webpack.DefinePlugin({
-			'process.env.MEDIATOR_JS_COV': JSON.stringify('development')
-		}),
-		//new BundleAnalyzerPlugin()
-	],
-	optimization: {
-		minimize: false
-	},
+		,
+		output: {
+			publicPath: '/',
+			path: path.resolve(__dirname, 'build'),
+			filename: '[name].js?v' + pkg.version
+		},
+		resolve: {
+			alias: {
+				root: path.resolve(__dirname, ''),
+				project: path.resolve(__dirname, './project'),
+				handlebars: 'handlebars/dist/handlebars.min.js'
+			},
+			fallback: {
+				crypto: require.resolve('crypto-browserify'),
+				stream: require.resolve('stream-browserify'),
+				buffer: require.resolve('buffer')
+			},
+		},
+		target: 'web',
+		module: {
+			rules: [
+				{
+					test: /\.html$/,
+					use: {
+						loader: 'html-loader',
+						options: {
+							minimize: !devMode
+						}
+					}
+				},
+				{
+					test: /\.worker\.js$/,
+					use: {
+						loader: 'worker-loader'
+					},
+				},
+				{
+					test: /.s?css$/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader'],
+				},
+				{
+					test: /\.hbs$/,
+					use: [{
+						loader: 'html-loader',
+						options: {
+							minimize: false
+						}
+					}]
+				},
+				{
+					test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+					type: 'asset/resource',
+				},
+			]
+		},
+		optimization: {
+			minimize: devMode ? false : true,
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						output: {
+							comments: /@license/i
+						},
+						warnings: true
+					},
+					extractComments: true,
+					parallel: true
+				}),
+				new CssMinimizerPlugin()
+			]
+			,
+			runtimeChunk: false
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: './src/index.html',
+				filename: './index.html',
+				chunks: ['mn']
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].css',
+				chunkFilename: '[id].css'
+			}),
+			new webpack.DefinePlugin({
+				'process.env.MEDIATOR_JS_COV': JSON.stringify('development')
+			}),
+			new CaseSensitivePathsPlugin()
+		],
+		stats: {
+			colors: true
+		},
+		devtool: devMode ? 'source-map' : false,
+		watchOptions: {
+			aggregateTimeout: 200,
+			poll: 1000
+		},
+		devServer: {
+			static: './build',
+			compress: true,
+			port: 9000,
+			hot: true,
+			client: {
+				overlay: false,
+			},
+		},
+		mode: devMode ? 'development' : 'production'
+	};
 };
